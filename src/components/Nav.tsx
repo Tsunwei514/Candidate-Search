d7a73fa50d6316772146242c7b1210fa80555e5c
Tsunwei514 <tristin.tsun@gmail.com>
2025-03-26 22:03:05 -0600

    add nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className='nav-container'>
+      <ul className="nav">
+        <li className="nav-item">
+          <NavLink className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')} to="/SavedCandidates">
+            Potential Candidates
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
