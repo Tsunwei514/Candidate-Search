6396175726c22ecfa5034a1c894630b5495e467b
Tsunwei514 <tristin.tsun@gmail.com>
2025-04-02 15:13:11 -0600

    add server config to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { env } from 'node:process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT as string, 10) || 3000,
+  },
 });
